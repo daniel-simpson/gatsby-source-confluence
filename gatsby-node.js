7cd591f702a851548e8215f7384e9ba30d18a4bc
Daniel Simpson <daniel_simpson1988@hotmail.com>
2019-03-18 14:31:57 +1100

    Add confluenceUrl property to allow backlinking

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,17 +8,20 @@ exports.sourceNodes = async (
   const { createNode } = actions
 
   // Get data from Confluence
-  const results = await getSearchResults(pluginOptions)
+  const response = await search(pluginOptions)
+
+  const baseUrl = response._links.base
+  const results = response.results.filter(result => result.type === 'page')
 
   // Parse into nodes and add to GraphQL schema
   results.forEach(pageResult => {
-    const node = formatPageNode(createNodeHelperFunctions, pageResult)
+    const node = formatPageNode(createNodeHelperFunctions, pageResult, baseUrl)
     //TODO: figure out how to set up page hierarchy
     createNode(node)
   })
 }
 
-const getSearchResults = async ({ hostname, auth, cql, limit = 10 }) => {
+const search = async ({ hostname, auth, cql, limit = 10 }) => {
   return await fetch(
     `https://${hostname}/wiki/rest/api/content/search/?cql=(${cql})&expand=body.storage,history,ancestors&limit=${limit}`,
     {
@@ -29,14 +32,17 @@ const getSearchResults = async ({ hostname, auth, cql, limit = 10 }) => {
     }
   )
     .then(x => x.json())
-    .then(x => x.results.filter(result => result.type === 'page'))
     .catch(e => {
       console.error('Unable to retrieve data from Confluence:', e)
       process.exit(1)
     })
 }
 
-const formatPageNode = ({ createNodeId, createContentDigest }, result) => {
+const formatPageNode = (
+  { createNodeId, createContentDigest },
+  result,
+  baseUrl
+) => {
   content = {
     confluenceId: result.id,
     title: result.title,
@@ -46,6 +52,7 @@ const formatPageNode = ({ createNodeId, createContentDigest }, result) => {
       email: result.history.createdBy.email,
     },
     bodyHtml: result.body.storage.value,
+    confluenceUrl: `${baseUrl}${result._links.webui}`,
   }
 
   const nodeId = createNodeId(`confluence-page-${content.confluenceId}`)
